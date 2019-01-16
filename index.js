d7e9b9e935676c23887316322e6bdce8086b52d6
Travis T <travistokuyama@gmail.com>
2019-01-16 00:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let currentUser = "me"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
-let excitedWelcome = "WELCOME TO FLATBOOK, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
